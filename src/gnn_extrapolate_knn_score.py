dd18af122140e08dce686c8181f0e014e805b59d
prasangadhungel <dhungel611@gmail.com>
2024-07-24 22:28:42 +0200

    extrapolate knn scores with gnn

--- src/gnn_extrapolate_knn_score.py
+++ src/gnn_extrapolate_knn_score.py
@@ -17,8 +17,10 @@ class GNN(torch.nn.Module):
     def __init__(self, input_dim, output_dim):
         super(GNN, self).__init__()
         self.conv1 = GCNConv(input_dim, 512)
-        self.conv2 = GCNConv(512, 256)
-        self.conv3 = GCNConv(256, output_dim)
+        self.conv2 = GCNConv(512, 512)
+        self.conv3 = GCNConv(512, 512)
+        self.conv4 = GCNConv(512, 256)
+        self.conv5 = GCNConv(256, output_dim)
 
     def forward(self, x, edge_index, edge_attr):
         x = self.conv1(x, edge_index, edge_attr)
@@ -26,13 +28,14 @@ class GNN(torch.nn.Module):
         x = self.conv2(x, edge_index, edge_attr)
         x = F.relu(x)
         x = self.conv3(x, edge_index, edge_attr)
+        x = F.relu(x)
+        x = self.conv4(x, edge_index, edge_attr)
+        x = F.relu(x)
+        x = self.conv5(x, edge_index, edge_attr)
         return x
 
 def get_edges_and_attributes(embeddings, k=10, distance="euclidean"):
-    if distance == "euclidean":
-        nbrs = NearestNeighbors(n_neighbors=k + 1, algorithm='ball_tree').fit(embeddings)
-    elif distance == "cosine":
-        nbrs = NearestNeighbors(n_neighbors=k + 1, algorithm='ball_tree').fit(embeddings)
+    nbrs = NearestNeighbors(n_neighbors=k + 1, algorithm='ball_tree').fit(embeddings)
         
     distances, neighbors = nbrs.kneighbors(embeddings)
     
@@ -40,31 +43,41 @@ def get_edges_and_attributes(embeddings, k=10, distance="euclidean"):
     edge_attr = []
 
     for i in range(neighbors.shape[0]):
-        for j in range(1, k + 1):  # Skip the first neighbor (itself)
+        for j in range(1, k + 1):
             edge_index.append([i, neighbors[i, j]])
-            edge_attr.append(distances[i, j])
+            edge_attr.append(1/distances[i, j])
             
     edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
     edge_attr = torch.tensor(edge_attr, dtype=torch.float)
     
     return edge_index, edge_attr
 
-def prepare_data(embeddings_dict, seed_samples, full_scores_dict, k, distance="euclidean"):
+def prepare_data(embeddings_dict, seed_samples, full_scores_dict, knn_extrapolated_score, k, distance="euclidean"):
     samples_list = [int(i) for i in embeddings_dict.keys()]
     
-    seed_samples_pos = [samples_list.index(i) for i in seed_samples]
+    train_samples_pos = [samples_list.index(i) for i in seed_samples]
 
     embeddings = np.array([embeddings_dict[i].cpu().numpy().flatten() for i in samples_list])
-    y = [full_scores_dict[str(i)] for i in samples_list]
-    y = torch.tensor(y, dtype=torch.float)
-    
+
     edge_index, edge_attr = get_edges_and_attributes(embeddings, k, distance)
-    x = torch.tensor(embeddings, dtype=torch.float)
+
+    true_scores = [full_scores_dict[str(i)] for i in samples_list]
+    binary_features = [1 if i in seed_samples else 0 for i in samples_list]
+
+    knn_scores = [knn_extrapolated_score[str(i)] for i in samples_list]
     
+    # use the knn scores as features     
+    node_features = torch.tensor(knn_scores, dtype=torch.float).unsqueeze(1)
+
+    # node_features = np.vstack([knn_scores, binary_features]).T
+    # node_features = torch.tensor(node_features, dtype=torch.float)
+    
+    y = torch.tensor(true_scores, dtype=torch.float)
+
     mask = torch.zeros(y.size(0), dtype=torch.bool)
-    mask[seed_samples_pos] = True
+    mask[train_samples_pos] = True
     
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y, train_mask=mask)
+    return Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr, y=y, train_mask=mask)
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -73,12 +86,12 @@ if __name__ == "__main__":
     with open("/nfs/homedirs/dhp/unsupervised-data-pruning/scores/SYNTHETIC_CIFAR100_1M_dynamic_uncertainty_train_2.json") as f:
         full_scores_dict = json.load(f)
 
-    with open("/nfs/homedirs/dhp/unsupervised-data-pruning/scores/SYNTHETIC_CIFAR100_50_dynamic_uncertainty_train_2.json") as f:
+    with open("/nfs/homedirs/dhp/unsupervised-data-pruning/scores/SYNTHETIC_CIFAR100_1M_dynamic_uncertainty_knn_extrapolated.json") as f:
         knn_extrapolated_score = json.load(f)
 
     model_combination = ["resnet50-trained-to-compute-score", "resnet18", "resnet50"]
-    k_combination = [5, 10, 50, 100]
-    num_seeds_combination = [10000, 20000]
+    k_combination = [10, 20, 50, 100]
+    trainset_size = [100_000, 50_000, 20_000, 10_000]
 
     results = []
 
@@ -101,23 +114,22 @@ if __name__ == "__main__":
 
         embeddings_dict = {}
         for i in tqdm(range(len(trainset)), mininterval=10, maxinterval=20):
-            if i % 2 == 0:
-                sample = trainset[i][0]
-                sample_idx = trainset[i][2]
-                sample = sample.to(device)
-                sample = sample.unsqueeze(0)
-                with torch.no_grad():
-                    embedding_val = embedding_model(sample)
-                embeddings_dict[sample_idx] = embedding_val.cpu()
+            sample = trainset[i][0]
+            sample_idx = trainset[i][2]
+            sample = sample.to(device)
+            sample = sample.unsqueeze(0)
+            with torch.no_grad():
+                embedding_val = embedding_model(sample)
+            embeddings_dict[sample_idx] = embedding_val.cpu()
         
         for k in tqdm(k_combination):
-            for num_seed in tqdm(num_seeds_combination):
+            for num_seed in tqdm(trainset_size):
                     wandb.init(
                             project=f"GNN extrapolate", name=f"k-{k}-num_seed-{num_seed}-model-{model_name}"
                     )
-                    seed_samples = np.random.choice(list(embeddings_dict.keys()), num_seed, replace=False)
+                    train_samples = np.random.choice(list(embeddings_dict.keys()), num_seed, replace=False)
                     distance_metric = "euclidean"
-                    data = prepare_data(embeddings_dict, seed_samples, full_scores_dict, k, distance_metric).to(device)
+                    data = prepare_data(embeddings_dict, train_samples, full_scores_dict, knn_extrapolated_score, k, distance_metric).to(device)
                     
                     model = GNN(input_dim=data.num_node_features, output_dim=1).to(device)
 
@@ -125,80 +137,79 @@ if __name__ == "__main__":
 
                     out = model(data.x, data.edge_index, data.edge_attr).squeeze()
 
-                    orig_seed = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
-                    pred_seed = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                    orig_train = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                    pred_train = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
 
-                    corr_seed = np.corrcoef(orig_seed, pred_seed)[0, 1]
-                    spearman_seed = spearmanr(orig_seed, pred_seed).correlation
+                    corr_train = np.corrcoef(orig_train, pred_train)[0, 1]
+                    spearman_train = spearmanr(orig_train, pred_train).correlation
 
-                    print(f"For seeded samples, Corr: {corr_seed}, Spearman: {spearman_seed}")
+                    print(f"For seeded samples, Corr: {corr_train}, Spearman: {spearman_train}")
 
-                    orig_list = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
-                    pred_list = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                    orig_test = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                    pred_test = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
                     
-                    corr_avg = np.corrcoef(orig_list, pred_list)[0, 1]
-                    spearman_avg = spearmanr(orig_list, pred_list).correlation
+                    corr_test = np.corrcoef(orig_test, pred_test)[0, 1]
+                    spearman_test = spearmanr(orig_test, pred_test).correlation
 
-
-                    print(f"For unseeded samples, Corr: {corr_avg}, Spearman: {spearman_avg}")
+                    print(f"For unseeded samples, Corr: {corr_test}, Spearman: {spearman_test}")
 
                     model.train()
-                    for epoch in range(10000):  # Number of epochs
+                    for epoch in range(5000):
                         optimizer.zero_grad()
                         out = model(data.x, data.edge_index, data.edge_attr).squeeze()
-                        loss = F.mse_loss(out[data.train_mask], data.y[data.train_mask])
+                        loss = F.mse_loss(out[~data.train_mask], data.y[~data.train_mask])
                         loss.backward()
                         optimizer.step()
-                        if epoch%100 == 0:
+                        if epoch % 100 == 0:
                             print(f"GNN Epoch: {epoch}, Loss: {loss.item()}")
     
                         wandb.log({"Train Loss": loss.item()}, step=epoch)
 
-                        orig_seed = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
-                        pred_seed = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                        orig_train = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                        pred_train = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
 
-                        corr_seed = np.corrcoef(orig_seed, pred_seed)[0, 1]
-                        spearman_seed = spearmanr(orig_seed, pred_seed).correlation
+                        corr_train = np.corrcoef(orig_train, pred_train)[0, 1]
+                        spearman_train = spearmanr(orig_train, pred_train).correlation
 
-                        wandb.log({"Corr Seed": corr_seed}, step=epoch)
-                        wandb.log({"Spearman Seed": spearman_seed}, step=epoch)
+                        wandb.log({"Corr Trainset": corr_test}, step=epoch)
+                        wandb.log({"Spearman Trainset": spearman_test}, step=epoch)
 
-                        orig_list = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
-                        pred_list = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                        orig_test = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                        pred_test = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
                         
-                        corr_avg = np.corrcoef(orig_list, pred_list)[0, 1]
-                        spearman_avg = spearmanr(orig_list, pred_list).correlation
+                        corr_test = np.corrcoef(orig_test, pred_test)[0, 1]
+                        spearman_test = spearmanr(orig_test, pred_test).correlation
 
-                        wandb.log({"Corr Unseed": corr_avg}, step=epoch)
-                        wandb.log({"Spearman Unseed": spearman_avg}, step=epoch)
+                        wandb.log({"Corr Testset": corr_train}, step=epoch)
+                        wandb.log({"Spearman Testset": spearman_train}, step=epoch)
 
                     model.eval()
                     with torch.no_grad():
                         out = model(data.x, data.edge_index, data.edge_attr).squeeze()
                     
-                    orig_list = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
-                    pred_list = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                    orig_test = data.y.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
+                    pred_test = out.cpu().detach().numpy()[~data.train_mask.cpu().detach().numpy()]
                     
-                    corr_avg = np.corrcoef(orig_list, pred_list)[0, 1]
-                    spearman_avg = spearmanr(orig_list, pred_list).correlation
+                    corr_test = np.corrcoef(orig_test, pred_test)[0, 1]
+                    spearman_test = spearmanr(orig_test, pred_test).correlation
                     
                     # check the correlation for the seed samples as well
-                    orig_seed = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
-                    pred_seed = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                    orig_train = data.y.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
+                    pred_train = out.cpu().detach().numpy()[data.train_mask.cpu().detach().numpy()]
 
-                    corr_seed = np.corrcoef(orig_seed, pred_seed)[0, 1]
-                    spearman_seed = spearmanr(orig_seed, pred_seed).correlation
+                    corr_train = np.corrcoef(orig_train, pred_train)[0, 1]
+                    spearman_train = spearmanr(orig_train, pred_train).correlation
 
-                    print(f"For seeded samples, Corr: {corr_seed}, Spearman: {spearman_seed}")
+                    print(f"For seeded samples, Corr: {corr_train}, Spearman: {spearman_train}")
                     results.append({
                         "model": model_name,
                         "k": k,
                         "num_seeds": num_seed,
-                        "corr_avg": corr_avg,
-                        "spearman_avg": spearman_avg,
+                        "corr_avg": corr_test,
+                        "spearman_avg": spearman_test,
                     })
                     wandb.finish()
-                    print(f"Model: {model_name}, k: {k}, Num Seeds: {num_seed}, Distance: {distance_metric} , Corr Avg: {corr_avg}, Spearman: {spearman_avg}")
+                    print(f"Model: {model_name}, k: {k}, Num Seeds: {num_seed}, Distance: {distance_metric} , Corr Avg: {corr_test}, Spearman: {spearman_test}")
 
     results_df = pd.DataFrame(results)
     results_df.to_csv("/nfs/homedirs/dhp/unsupervised-data-pruning/data/gnn_extrapolation.csv", index=False)
